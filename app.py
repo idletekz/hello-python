cd9d6afc2092f97f55b8732660284874d7349efc
alex <idletekz@gmail.com>
2023-11-18 21:37:27 -0500

    base

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template
+from featureflags.client import CfClient, Target
+
+app = Flask(__name__)
+
+client = None
+api_key = os.getenv('FF_API_KEY')
+client = CfClient(api_key)
+
+@app.route('/')
+def hello_world():
+    target = Target(identifier='alex', name="alex")
+    message = client.string_variation("message", target, "hello, world")
+    return render_template('hello.html', message=message)
+
+def handle_app():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    handle_app()
+    client.close();
